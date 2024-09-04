5bce28c546c3de36fcd18aa37a0cd25438448b2e
Alexander Matthew Marinbakh <amarinbakh0@saddleback.edu>
2024-09-04 03:42:29 +0000

    sep3

---
+++ 6.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+int main() {
+int number;
+cout << "number: " << endl;
+cin >> number;
+int remainder = number % 2;
+cout << "remainder: " << remainder << endl;
+}
