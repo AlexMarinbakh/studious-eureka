3209af44bad8aad6f58a13eee85755aa52dbc4eb
Alexander Matthew Marinbakh <amarinbakh0@saddleback.edu>
2024-09-04 03:41:07 +0000

    sep3

---
+++ 2.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main() {
+int x; 
+// we ask c++ to get a memory location from RAM and name it x
+x = 10;
+// we ask c++ to store a value of 10 in memory location named x
+int y; 
+// another memory location from RAM named y
+cout << "x: " << x << endl;
+cout << "y: " << y << endl;
+}
