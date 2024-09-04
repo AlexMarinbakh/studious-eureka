a9306debae863b5963d2055093715557c0d2425b
Alexander Matthew Marinbakh <amarinbakh0@saddleback.edu>
2024-09-04 03:42:16 +0000

    sep3

---
+++ 4.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main() {
+int a;
+cout << "a: " << endl;
+cin >> a;
+int b;
+cout << "b: " << endl;
+cin >> b;
+int c;
+cout << "c: " << endl;
+cin >> c;
+int d = (a + b) * c;
+cout << "d: " << d << endl;
+}
