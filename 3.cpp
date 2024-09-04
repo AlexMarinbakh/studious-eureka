cc8b4646a2c0629bbeb3b9d8b76ee40fd248a7e8
Alexander Matthew Marinbakh <amarinbakh0@saddleback.edu>
2024-09-04 03:42:07 +0000

    sep3

---
+++ 3.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main() {
+int x;
+cout << "x: " << endl; // printing the stored number
+cin >> x; // reading a value from console out and storing it
+int y;
+cout << "y: " << endl;
+cin >> y;
+int z = x + y;
+cout << z << endl;
+}
