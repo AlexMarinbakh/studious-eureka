346174a094ae5ede985a60881c33f505791200f1
Alexander Matthew Marinbakh <amarinbakh0@saddleback.edu>
2024-09-06 03:48:02 +0000

    sep5

---
+++ p4.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main() {
+int length;
+int width;
+cout << "length: ";
+cin >> length;
+cout << "width: ";
+cin >> width;
+int area = length * width;
+cout << area << endl;
+}
